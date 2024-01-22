7ee71d990e29070f879221e143150de093ea697b
Olowoyeye Iretomiwa <olowoyeyetomiwa86@gmail.com>
2024-01-22 15:25:33 +0100

    first commit

---
+++ src/MarvelData.js
@@ -0,0 +1,95 @@
+export const characters = [
+  // Iron Man
+  {
+    name: "Iphone 15",
+    alias: "Iphone 15",
+    img1: "https://i.pinimg.com/564x/bf/f4/2d/bff42d2af8e6125ae5973f226144aeb2.jpg",
+    homeworld: "2023",
+    abilities: [
+      "Titanium alloy frame",
+      "New Type C port",
+      "Shoots 24MP standard photos",
+    ],
+    team: "Iphone",
+    description:
+      "The iPhone is a smartphone made by Apple that combines a computer, iPod, digital camera and cellular phone into one device with a touchscreen interface. The iPhone runs the iOS operating system, and in 2021 when the iPhone 13 was introduced, it offered up to 1 TB of storage and a 12-megapixel camera.",
+  },
+  {
+    name: "Samsung",
+    alias: "Samsung Galaxy S24 Ultra",
+    abilities: ["AI Built into it", "Brightest screen in a smartphone"],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/ea/8e/7c/ea8e7c1802704c1e6787c3ddd2f96e7f.jpg",
+    homeworld: "2024",
+    description:
+      "The Samsung Galaxy S24 Ultra comes with 6.8-nch Dynamic AMOLED display with 120Hz refresh rate and Qualcomm Snapdragon 8 Gen 3 processor",
+  },
+  {
+    name: "Google",
+    alias: "Google Pixel 8 Plus",
+    abilities: ["Android 14"],
+    team: "Android",
+    img1: "https://i.pinimg.com/736x/86/ea/99/86ea99811c9f8d54213075d725594eb8.jpg",
+    homeworld: "2023",
+
+    description:
+      "its Google-powered software and AI image editing tools. The smartphone features the new Google Tensor G3 chip, a brighter screen, and an updated camera triple array with tons of advanced photo and video modes.",
+  },
+  {
+    name: "Redmi",
+    alias: "Redmi Note 13 pro",
+    abilities: ["Octa-core (4x2.40 GHz Cortex-A78 & 4x1.95 GHz Cortex-A55)"],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/4b/c8/b9/4bc8b9e887e17b2fb73119f1c5db5709.jpg",
+    homeworld: "2023",
+
+    description:
+      "ProcessorSnapdragon® 6856nm manufacturing process technologyCPU: Octa-core CPU, up to 2.8GHzGPU: Adreno 610. Storage & RAM6GB+128GB | 8GB+128GB | 8GB+256GB*LPDDR4X + UFS2. ... DimensionsHeight: 162.24mmWidth: 75.55mmThickness: 7.97mmWeight: 188.5g*Data provided by internal laboratories.",
+  },
+
+  // Spider-Man
+  {
+    name: "Samsung",
+    alias: "Samsung Z fold  5",
+    abilities: ["Open up to a bigger screen"],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/3c/7f/56/3c7f56f4f8c646afb41a33e1b9b8affa.jpg",
+    homeworld: "2023",
+    description:
+      "The Samsung Galaxy Z Fold 5 is a dual-SIM (GSM and GSM) mobile that accepts Nano-SIM and Nano-SIM cards. The Samsung Galaxy Z Fold 5 measures 129.90 x 154.90 x 6.10mm (height x width x thickness) and weighs 253.00 grams.",
+  },
+
+  {
+    name: "Google",
+    alias: "Pixel Fold",
+    abilities: ["Open up and get a bigger screen"],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/32/96/33/329633cb16a29f099c091bf1f455d90c.jpg",
+    homeworld: "2023",
+    description:
+      "A full 5.8 Pixel phone when it's folded. Unfolded, you get a 7.6 immersive display. The most durable hinge on a foldable. And the thinnest foldable of all.",
+  },
+
+  {
+    name: "ASUS",
+    alias: "Asus Rog Phone 7",
+    abilities: ["Built in cooling fan to ensure maximum gaming experience"],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/e3/a2/27/e3a227634d354c44a7a592006985221b.jpg",
+    homeworld: "2023",
+    description:
+      "ROG was founded with the goal of creating the world's most powerful and versatile gaming laptops in the industry. Our premium devices elevate gaming experiences with best-in-class displays, top tier graphics, and innovative cooling solutions that take performance to new heights.",
+  },
+  {
+    name: "Nothing",
+    alias: "Nothing Phone 2",
+    abilities: ["Glyph Progress integration for Google Calendar, improved Glyph Timer functionality, and support for the predictive back gesture for all Nothing apps."],
+    team: "Android",
+    img1: "https://i.pinimg.com/564x/29/02/6c/29026c4e985d17fd3d572ab6a0aef7b9.jpg",
+    homeworld: "2023",
+    description:
+      "Sam Wilson, the Falcon, soars through the skies with his high-tech wingsuit and possesses a unique telepathic link with birds. As an Avenger, Sam exemplifies freedom, hope, and unity, fighting alongside his fellow heroes to maintain peace and protect the world from impending threats.",
+  },
+];
+
+console.log(characters);
